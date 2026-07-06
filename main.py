1bd0a092ecf248aa5d28dadfb31d34d992d2cbc8
hseyinblgc <h.bilgic1818@gmail.com>
2026-07-06 17:11:36 +0300

    Initial commit
    feat: implement OpenRouter integration

---
+++ main.py
@@ -0,0 +1,51 @@
+import argparse
+import os
+
+from dotenv import load_dotenv
+from openai import OpenAI
+from openai.types.chat import ChatCompletion
+
+load_dotenv()
+api_key: str | None = os.environ.get("OPENROUTER_API_KEY")
+if api_key is None:
+    raise RuntimeError("Error: You need to set OPENROUTER_API_KEY in your .env ")
+
+parser = argparse.ArgumentParser(description="Chatbot")
+parser.add_argument("user_prompt", type=str, help="User prompt")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+args = parser.parse_args()
+# Now we can access `args.user_prompt`
+
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=api_key,
+)
+
+messages: list[dict[str, str]] = [
+    {"role": "user", "content": args.user_prompt},
+]
+response: ChatCompletion = client.chat.completions.create(
+    model="openrouter/free",
+    messages=messages,
+)
+
+
+def formatter(obj: ChatCompletion, userprompt: str, verbose: bool) -> str:
+    if obj.usage is None:
+        raise RuntimeError("usage property is None.")
+    if verbose:
+        return f"""
+        User prompt: {userprompt}
+        Prompt tokens: {obj.usage.prompt_tokens}
+        Response tokens: {obj.usage.completion_tokens}
+        Response:
+        {obj.choices[0].message.content}"""
+    return f"Response:\n{obj.choices[0].message.content}"
+
+
+def main():
+    print(formatter(response, args.user_prompt, args.verbose))
+
+
+if __name__ == "__main__":
+    main()
